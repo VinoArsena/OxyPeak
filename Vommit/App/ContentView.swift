05dd65e14617fef4bf94b5f95fd01b6fd6c1198d
Christopher Hardy Gunawan <christopher.hygunawan@gmail.com>
2026-04-30 09:08:54 +0700

    fix: update content view

--- Vommit/App/ContentView.swift
+++ Vommit/App/ContentView.swift
@@ -5,17 +5,27 @@ struct ContentView: View {
 
     var body: some View {
         Text("VOMMIT")
-            .font(.largeTitle.bold())
+            .bold()
+            .font(.largeTitle)
+            .padding()
         
-        Text("Check minimum VO₂ Max \n to safely summit")
-            .font(.callout)
+        Text("Check minum **VO\u{2082}Max** \nto safely summit")
             .multilineTextAlignment(.center)
+            .padding()
         
+        Image(systemName: "mountain.2")
+            .font(.largeTitle)
+            .padding()
         
+        Text("Tap here to start")
         
-        
+        Text("Based on research from\nAmerican College of Sports Medicine")
+            .multilineTextAlignment(.center)
+            .fontWeight(.thin)
+            .italic()
+            .font(.caption)
+            .padding()
     }
-
 }
 
 #Preview {
