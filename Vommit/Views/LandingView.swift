aa00a6ffc2ec3ee14035deb25d10106ba95080cd
Vino Arsena <116153073+VinoArsena@users.noreply.github.com>
2026-05-04 08:41:27 +0700

    mod: healthkit settings

--- Vommit/Views/LandingView.swift
+++ Vommit/Views/LandingView.swift
@@ -3,38 +3,43 @@ import SwiftUI
 struct LandingView: View {
     
     var body: some View {
-        
-        ZStack {
-            VStack {
-                Image(systemName: "mountain.2")
-                    .font(.largeTitle)
-                    .padding(24)
-                
-                Text("VOMMIT")
-                    .font(.largeTitle.bold())
-                
-                Spacer()
-                
-                Text("Check minimum **VO₂ Max** \nto safely summit")
-                    .multilineTextAlignment(.center)
-                
-                Spacer()
-                
-                Text("Tap here to start")
-                
-                Spacer()
-                
-                Text("Based on research from\nAmerican College of Sports Medicine")
-                    .multilineTextAlignment(.center)
-                    .fontWeight(.thin)
-                    .font(.caption.italic())
+        NavigationStack {
+            ZStack {
+                VStack {
+                    Image(systemName: "mountain.2")
+                        .font(.largeTitle)
+                        .padding(24)
+                    
+                    Text("VOMMIT")
+                        .font(.largeTitle.bold())
+                    
+                    Spacer()
+                    
+                    Text("Check minimum **VO₂ Max** \nto safely summit")
+                        .multilineTextAlignment(.center)
+                    
+                    Spacer()
+                    
+                    NavigationLink {
+                        HealthSyncTestView()
+                    } label: {
+                        Text("Tap here to start")
+                    }
+                    
+                    
+                    Spacer()
+                    
+                    Text("Based on research from\nAmerican College of Sports Medicine")
+                        .multilineTextAlignment(.center)
+                        .fontWeight(.thin)
+                        .font(.caption.italic())
+                }
+                .padding(24)
+                .frame(maxWidth: .infinity)
             }
-            .padding(24)
-            .frame(maxWidth: .infinity)
+            .preferredColorScheme(.dark)
+            .background(Color("Background"))
         }
-        .preferredColorScheme(.dark)
-        .background(Color("Background"))
-        
     }
 }
 
