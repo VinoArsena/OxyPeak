252bc4fda649341ab5acde83f1d8d1ad15078a85
Christopher Hardy Gunawan <christopher.hygunawan@gmail.com>
2026-05-05 21:14:26 +0700

    mod: update HomeView.swift

--- Vommit/Views/HomeView.swift
+++ Vommit/Views/HomeView.swift
@@ -1,89 +1,91 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State private var searchText: String = ""
-    @State private var chosenGrade: Int = 0
-    @State private var navigationId = UUID()
-    
     let mountains: [Mountain] = DatabaseManager.mountains
-    let grades = [
-        (label: "All", value: 0),
-        (label: "Grade 1", value: 1),
-        (label: "Grade 2", value: 2),
-        (label: "Grade 3", value: 3),
-        (label: "Grade 4", value: 4),
-        (label: "Grade 5", value: 5)
-    ]
-    
-    var filteredMountains: [Mountain] {
-        mountains.filter { mountain in
-            let searchTextMatch = searchText.isEmpty || mountain.name.localizedCaseInsensitiveContains(searchText)
-            
-            let gradeMatch = chosenGrade == 0 || mountain.grade == chosenGrade
-            
-            return searchTextMatch && gradeMatch
-        }
-    }
+    @State private var navigate = false
     
     var body: some View {
-        TabView {
-            ZStack {
-                NavigationStack {
-                    Picker("Filter Grades", selection: $chosenGrade) {
-                        ForEach(grades, id: \.value) { option in
-                            Text(option.label).tag(option.value)
-                        }
+        VStack(alignment: .leading) {
+            // Welcome Text
+            VStack(alignment: .leading) {
+                Text("Hi Adri")
+                    .font(.largeTitle)
+                    .bold()
+                
+                Text("Ready to Hike today?")
+            }
+            
+            // Profile Card
+            HStack {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Joanne Doe")
+                        .font(.system(size: 20))
+                        .bold()
+                    Text("Others")
+                    Text("18 years old")
+                    Text("170 cm")
+                    Text("60 kg")
+                }
+                .padding(.leading, 25)
+                
+                Spacer()
+                
+                Divider()
+                    .frame(height: 175)
+                
+                Spacer()
+                
+                VStack(spacing: 8) {
+                    Text("VO\u{2082} Max")
+                        .bold()
+                    Text("20.8")
+                        .font(.system(size: 32))
+                        .bold()
+                    Text("ml/kg/min")
+                    Button {
+                        // sync
+                    } label: {
+                        Text("Update")
                     }
-                    .pickerStyle(.segmented)
-                    .padding(.horizontal)
-                    .background(Color.background)
-                    
-                    ScrollView {
-                        ForEach(filteredMountains, id: \.id) { mountain in
-                            NavigationLink {
-                                MountainDetailView(mountain: mountain, gradeColorTheme: .orange)
-                            } label: {
-                                MountainCard(mountain: mountain)
-                                    .padding(.bottom, 5)
-                            }
-                            .buttonStyle(.plain)
+                    .buttonStyle(.glass)
+                }
+                .padding(.trailing, 35)
+            }
+            .padding(16)
+            .frame(maxWidth: .infinity)
+            .background(Color.cardBackground)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            
+            // Mountain Cards
+            VStack(alignment: .leading) {
+                Text("Pick Mountain")
+                    .font(.largeTitle)
+                    .bold()
+                
+                ForEach(mountains, id: \.id) { mountain in
+                    HStack{
+                        Button {
+                            navigate = true
+                        } label: {
+                            Image(systemName: "mountain.2")
+                            Text("Mt. " + mountain.name)
                         }
+                        .buttonStyle(.plain)
+                        .navigationDestination(isPresented: $navigate) {
+//                            MountainDetailView(mountain: mountain)
+                        }
+                        
+                        Spacer()
                     }
                     .padding(16)
-                    .navigationTitle("Choose Mountain")
-                    .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
-                    .background(Color.background)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.cardBackground)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
                 }
-                .id(navigationId)
-                .onReceive(NotificationCenter.default.publisher(for: NSNotification.Name("PopToRoot"))) { _ in
-                    navigationId = UUID()
-                }
-                
-                .tabItem {
-                    Label("Profile", systemImage: "person")
-                }
-            }
-            .preferredColorScheme(.dark)
-            .background(Color("Background"))
-            
-            .tabItem {
-                Label("Home", systemImage: "house.fill")
-            }
-            
-            ProfileView(
-                user: User(
-                    name: "Joanne Doe",
-                    dob: Date(),
-                    gender: .others,
-                    height: 170,
-                    weight: 60,
-                    vo2Max: 30
-                ))
-            .tabItem {
-                Label("Profile", systemImage: "person")
             }
         }
-        .background(Color.background)
+        .padding(16)
+        .preferredColorScheme(.dark)
     }
 }
 
